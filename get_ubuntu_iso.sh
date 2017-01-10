3100a78352ce22aa7e71ed27f5fbb452a8eb3c64
Tom Weatherhead <tom_weatherhead@hotmail.com>
2017-01-10 07:25:21 -0500

    Added get_ubuntu_iso.sh

---
+++ get_ubuntu_iso.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+See https://www.ubuntu.com/download/how-to-verify
+List of Ubuntu mirrors: https://launchpad.net/ubuntu/+cdmirrors
+
+- Verify Ubuntu .iso download: (Write as a script? : 1) Replace 16.10 with $1 ; 2) Don't try to recv-keys if they are already present in the local keyring)
+  - gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys "8439 38DF 228D 22F7 B374 2BC0 D94A A3F0 EFE2 1092" "C598 6B4F 1257 FFA8 6632 CBA7 4618 1433 FBB7 5451"
+  - gpg --list-keys --with-fingerprint 0xFBB75451 0xEFE21092
+  - curl -O https://mirror.csclub.uwaterloo.ca/ubuntu-releases/16.10/SHA256SUMS
+  - curl -O https://mirror.csclub.uwaterloo.ca/ubuntu-releases/16.10/SHA256SUMS.gpg
+  - gpg --verify SHA256SUMS.gpg SHA256SUMS
+  - curl -O https://mirror.csclub.uwaterloo.ca/ubuntu-releases/16.10/ubuntu-16.10-desktop-amd64.iso
+  - sha256sum -c SHA256SUMS 2>&1 | grep OK
