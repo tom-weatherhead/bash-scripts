f2fde2de85cdb85ef8e52a59624ea859655c0001
Tom Weatherhead <tom_weatherhead@hotmail.com>
2017-01-14 23:05:16 -0700

    Improvements

--- ip_addr_win.sh
+++ ip_addr_win.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-# echo
+# ip_addr_win.sh : Print the IPv4 addresses, MAC addresses, and descriptions of all active network interfaces, listed in their preferred order.
 
-# ipconfig /all | grep -e Description -e Physical -e IPv4 | perl -nle 'sub outp { my $A=shift; print "$A = $P = $D"; $P=$D=""; } outp $1 if /^\s+IPv4 Address.+\:\s(.+)[$(]/ ; $D=$1 if /^\s+Description.+\:\s(.+)$/ ; $P=$1 if /^\s+Physical.+\:\s(.+)$/'
+# Hexadecimal digit: [0-9a-fA-F] -> [[:xdigit:]]
 
-# echo
-
-# netstat -rn | perl -nle 'print "$1 = $2 = $5" if /^(\s{1,2}[0-9]{1,2})\.{3}(([0-9a-fA-F]{2}\s){5}([0-9a-fA-F]{2}))\s\.+(.+)$/' | sort -k1,3 | perl -nle 'print "$1" if /^.{6}(.{17})/' | tr [a-f\ ] [A-F-]
-
-# echo
-
-netstat -rn | perl -nle 'print "$1 = $2 = $5" if /^(\s{1,2}[0-9]{1,2})\.{3}(([0-9a-fA-F]{2}\s){5}([0-9a-fA-F]{2}))\s\.+(.+)$/' | sort -k1,3 | perl -nle 'print "$1" if /^.{6}(.{17})/' | tr [a-f\ ] [A-F-] | while read -r line ; do
-	ipconfig /all | grep -e Description -e Physical -e IPv4 | perl -nle 'sub outp { my $A=shift; print "$A = $P = $D"; $P=$D=""; } outp $1 if /^\s+IPv4 Address.+\:\s(.+)[$(]/ ; $D=$1 if /^\s+Description.+\:\s(.+)$/ ; $P=$1 if /^\s+Physical.+\:\s(.+)$/' | grep $line
+netstat -rn | perl -nle 'print "$1 = $2 = $5" if /^( {1,2}[0-9]{1,2})\.{3}(([[:xdigit:]]{2} ){5}([[:xdigit:]]{2})) \.+(.+)$/' | sort -k1,3 | perl -nle 'print $1 if /^.{6}(.{17})/' | tr [a-f\ ] [A-F-] | while read -r line; do
+	ipconfig /all | grep -e Description -e Physical -e IPv4 | perl -nle 'sub word_up { my $A=shift; print "$A = $P = $D"; $P=$D=""; } word_up $1 if /^ +IPv4 Address.+: (.+)[$(]/ ; $D=$1 if /^ +Description.+: (.+)$/ ; $P=$1 if /^ +Physical.+: (.+)$/' | grep $line
 done
