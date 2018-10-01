6964ed768d130f14db695d81cfd0946c1727ac33
Tom Weatherhead <tom_weatherhead@hotmail.com>
2018-10-01 10:07:34 -0600

    Improved the Git backup script

--- cron_git_server.sh
+++ cron_git_server.sh
@@ -5,16 +5,11 @@
 # Run this as a cron job every 5 minutes by editing your crontab (crontab -e) and adding a line like this:
 # */5 * * * * /path/to/cron_git_server.sh
 
-# NOW=$($(/usr/bin/env date) --utc +%Y-%m-%d_%H-%M-%S)
+SRCDIR="$1"
+DESTDIR="$2"
 NOW=$(date --utc +%Y-%m-%d_%H-%M-%S)
-
-mkdir -p /srv/gitbackups
-# cd /srv/git
-# $(/usr/bin/env tar) cjvf "/srv/gitbackups/git_backup_$NOW.tar.bz2" *
-# tar cjvf "/srv/gitbackups/git_backup_$NOW.tar.bz2" *
+mkdir -p "$DESTDIR"
 # Use "." instead of "*" because "*" is expanded before -C changes the directory; see https://unix.stackexchange.com/questions/199038/tar-unix-not-changing-directory
-tar -cjvf "/srv/gitbackups/git_backup_$NOW.tar.bz2" -C /srv/git .
-chown -R tomw:tomw /srv/gitbackups
-
-# Delete all *.bz2 files except for the most recent three:
-ls -At /srv/gitbackups/*.bz2 | tail -n +4 | xargs rm -- 2>/dev/null
+tar -cjf "$DESTDIR/git_backup_$NOW.tar.bz2" -C "$SRCDIR" .
+# Don't put double quotes around $DESTDIR/git_backup_*.tar.bz2 :
+ls -At $DESTDIR/git_backup_*.tar.bz2 | tail -n +4 | xargs rm -- 2>/dev/null
